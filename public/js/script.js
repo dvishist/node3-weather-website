99aebe3bc36adaff5cc30ebdceb14a0fb4d393bb
dvishist <54056185+dvishist@users.noreply.github.com>
2020-06-07 17:28:37 +1000

    add loading text during weather fetch

--- public/js/script.js
+++ public/js/script.js
@@ -9,6 +9,11 @@ const resultDiv = document.querySelector('.result')
 document.getElementById('share-location').addEventListener('click', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(pos => {
+            resultDiv.hidden = false;
+            messageBody.textContent = "Loading..."
+            weatherImg.src = ''
+            locationTime.textContent = ''
+            messageTitle.textContent = ''
             fetch(`/forecast?lat=${String(pos.coords.latitude)}&lon=${String(pos.coords.longitude)}`)
                 .then(response => {
                     response.json().then(data => {
